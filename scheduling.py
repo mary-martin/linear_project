7952c2e499852e5b5d2949c42e0432676813fb57
Mary Martin <mary.martin@colorado.edu>
2020-04-30 11:06:01 -0600

    add to scheduling and fix some issues

--- scheduling.py
+++ scheduling.py
@@ -1,15 +1,30 @@
 import data as db
 import sys
-from ortools.constraint_solver import pywrapcp
+from ortools.sat.python import cp_model
 
-def schedule_event():
 
-    solver = pywrapcp.Solver("staffing")
+def schedule_week(week_of):
+
+    # set solver to CP Solver
+    model = cp_model.CpModel()
+
+    # get specified events
+    events = db.get_weekly_events(week_of)
     employees = db.get_employee_list()
-    # num_employees = event.num_employees
+    num_employees = event['num_employees']
 
-    # constraints on hours worked per week < 20
-    print(employees)
+    # 2 hr intervals starting at 6 am
+    # event intervals
+    for event in events:
+        
+
+     
+    
     # print(event)
     
-schedule_event()
+def set_schedule(employees):
+    db.staff_event(employees)
+    print(schedule_set)
+
+def show_solutions():
+    return 
