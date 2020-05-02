dcc70a30e1e8829bfbc349fdb1afa9ab46ee2bc7
Mengchen Gong <mego6698@colorado.edu>
2020-05-01 23:44:03 -0600

    Add files via upload

--- data.py
+++ data.py
@@ -14,7 +14,7 @@ def add_event(name, date, time, duration, evt_type, num_employees):
      
     event = {
         'event_name': name,
-        'date': date,
+        'week_of': date,
         'time': time, 
         'duration': duration,
         'type': evt_type, 
@@ -33,24 +33,35 @@ def add_employee(name, can_manage, event_pref):
     employee = {
         'name': name,
         'can_manage': can_manage,
-        'event_pref': event_pref,
-        'available_hrs_id': "",
+        'event_pref': event_pref
     }
     result = db.Employees.insert_one(employee)
-    print('employee added: ', name)
+    print('Employee added: ', name)
 
-def set_availability(hrs, name):
     
-    # fix this, rough draft 
-    employee = db.Employees.find_one({'name': name})
-    availability = hrs
+    
+def set_availability( hrs):
     # update for each day of the week 
-    db.Available_hours.insert_one({'available_hrs': hrs, 'employee_id': employee.get('_id')})
+    # employee = db.Employees.find_one({'name': name})
+    availability = {
+        'employee_id': "",
+        'Sunday': hrs[:, 0].tolist(),
+        'Monday': hrs[:, 1].tolist(),
+        'Tuesday': hrs[:, 2].tolist(),
+        'Wednesday': hrs[:, 3].tolist(),
+        'Thursday': hrs[:, 4].tolist(),
+        'Friday': hrs[:, 5].tolist(),
+        'Saturday': hrs[:, 6].tolist()
+    }
+    # db.employees.update_one({'name': employee.get('name')}, { '$set': { 'unavailability': availability } })
+    db.Available_hours.insert_one(availability)
+    print("Employee hours have been set.")
+    print(availability)
 
 def change_event_pref(name, pref):
 
     employee = db.Employees.find_one({'name': name})
-    db.employees.update_one({'_id': { eq: employee.get('_id') } }, { set: { 'event_pref': pref } })
+    db.employees.update_one({'_id': employee.get('_id')}, { '$set': { 'event_pref': pref } })
 
 def get_event(event_name):
 
@@ -59,7 +70,7 @@ def get_event(event_name):
 
 def set_employee_hrs(hours):
 
-    db.employees.update_one({'_id': employee.get('_id') }, { set: { 'event_pref': pref } })
+    db.employees.update_one({'_id': employee.get('_id') }, { '$set': { 'event_pref': pref } })
 
 def get_employee_list():
     
@@ -78,3 +89,29 @@ def get_employee_count():
     employee_count = len(list(db.Events.find({})))
     print("Employee count: ", employee_count)
     return employee_count
+
+def staff_event(employees, event_name):
+    event = get_event(event_name)
+    db.Events.update_one({'_id':  event.get('_id')  }, { '$set': { 'staff': employees} })
+
+def get_weekly_hours(employee_id):
+    return db.Weekly_hours.find_one({'employee_id': employee_id, 'week_of': week_of})['hours']
+
+def update_weekly_hours(employee_id, week_of, hrs):
+    hrs_td = db.Weekly_hours.find_one({'employee_id': employee_id, 'week_of': week_of})
+    total = hrs_td['hours']
+    _id = hrs_td['_id']
+    db.Weekly_hours.update_one({'_id': _id},{'$set': {'hours': (total+hrs)}})
+
+def get_availability(day, employee_id):
+    availability = db.Available_hours.find_one({'employee_id': employee_id})
+    print(availability)
+    print(day)
+    return availability[str(day)]
+
+def find_avail(name):
+    employee = db.Employees.find_one({'name': name})
+    last_entry = db.Available_hours.find().sort([('timestamp', -1)]).limit(1)[0]
+    _id = last_entry['_id']
+    db.Available_hours.update_one({'_id': _id},{'$set': {'employee_id': employee['_id']}})
+
--- scheduling.py
+++ scheduling.py
@@ -1,15 +1,118 @@
 import data as db
 import sys
-from ortools.constraint_solver import pywrapcp
+from ortools.sat.python import cp_model
+import datetime
+from random import randint
 
-def schedule_event():
+global EMPLOYEE_HOURLY_LIMIT
+EMPLOYEE_HOURLY_LIMIT = 10
 
-    solver = pywrapcp.Solver("staffing")
+def schedule_week():
+    # maximize based on preferred event type 
+    # set solver to CP Solver
+    model = cp_model.CpModel()
+
+    # get specified events
+    events = db.get_event_list()
+    num_events = len(db.get_event_list())
+
     employees = db.get_employee_list()
-    # num_employees = event.num_employees
 
-    # constraints on hours worked per week < 20
-    print(employees)
-    # print(event)
+    # 2 hr intervals starting at 6 am
+    # event intervals
+    shifts = {}
+    event_preferences =  []
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    all_managers = []
+    all_avail = []
+
+    for n, employee in enumerate(employees):
+        employee_id = employee['_id']
+        avail = []
+        can_manage = []
+        for e, event in enumerate(events):
+            time = event['time']
+            hours = float(time[0:2])
+            mins = float(time[3:5])
+            print('hours ', hours)
+            start_time = hours + mins/60
+            duration = float(event['duration'])
+            end_time = start_time + duration
+            week_of = event['week_of']
+
+            time_slot = generate_time_slots(start_time, end_time)
+            weekday = get_day_of_week(event['week_of'])
+            duration = range(int(duration))
+            print(employee_id)
+            availability = db.get_availability(days[weekday], employee_id)
+            event_pref = []
+            can_manage = employee['can_manage']
+
+            available = 1
+            for n, slot in enumerate(time_slot):
+                if slot == 1 and availability[n] == 0:
+                    available = 0
+
+            model.Add(available == 1)
+
+            for h in duration:
+                shifts[(n, e, h)] = model.NewBoolVar('eventshift_n%ie%ih%i' % (n, e))
+                event_pref.append(pref)
+
+            model.Add(sum(shifts[(n, e, h)] for h in duration) + db.get_weekly_hours(employee_id, week_of) <= EMPLOYEE_HOURLY_LIMIT)
+        event_preferences.append(event_pref)
+
+    for e, event in enumerate(events):
+        model.Add(sum(shifts[(n, e, h)] for n in range(num_employees)) == event['num_employees'])
     
-schedule_event()
+    model.Maximize(
+        sum(event_preferences[n][e][h] * shifts[(n, e, h)] for n in range(num_employees) for e in num_employees for h in duration))
+
+    solver.solve(model)
+    show_solutions(solver, shifts, events, employees, duration, event_preferences)
+    
+def set_schedule(employees):
+    db.staff_event(employees)
+
+def show_solutions(shifts, events, employees, duration, event_pref):
+
+    for n, employee in enumerate(employees):
+        for e, event in enumerate(events):
+            for h in duration: 
+                if solver.Value(shifts[(n, e, h)]) == 1:
+                    if event_pref[n][e][h] == 1:
+                        print('Employee: ', employee['name'], 'Works event: ', event['event_name'], ' (preferred)')
+                    else: 
+                        print('Employee: ', employee['name'], 'Works event: ', event['event_name'], ' (not preferred)')
+    return 
+
+def get_day_of_week(date):
+    month = int(date[0:2])
+    day = date[3:5]
+    year = date[6:10]
+    # datetime.date(year, month, day).weekday()
+    return randint(0,6)
+
+
+def generate_time_slots(start, end):
+    day = [0] * 12
+
+    time = 6.0
+    for n in range(12):
+        if start >= time and start < (time + 2):
+            day[n] = 1
+            i = n
+        if end <=  (time + 2) and end > time:
+            day[n] = 1
+            j = n
+        time += 2.0
+        if time > 24:
+            time = 0
+
+    for n in range(12):
+        if n > i and n < j:
+            day[n] = 1
+    print(day)
+    return day 
+
+        
